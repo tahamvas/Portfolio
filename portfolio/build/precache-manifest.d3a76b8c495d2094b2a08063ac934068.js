4dce8c6d7be9c44445824123caf52742a8ae8f04
Tyler Hamvas <97547652+tahamvas@users.noreply.github.com>
2022-05-17 10:28:53 -0400

    Added Build

---
+++ portfolio/build/precache-manifest.d3a76b8c495d2094b2a08063ac934068.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "24c2c451605b1083641d",
+    "url": "/static/css/main.0a9057d5.chunk.css"
+  },
+  {
+    "revision": "24c2c451605b1083641d",
+    "url": "/static/js/main.24c2c451.chunk.js"
+  },
+  {
+    "revision": "17fb29ed40cd8c1083bf",
+    "url": "/static/js/1.17fb29ed.chunk.js"
+  },
+  {
+    "revision": "cb44da0322ae3a80da71",
+    "url": "/static/css/2.e6756b72.chunk.css"
+  },
+  {
+    "revision": "cb44da0322ae3a80da71",
+    "url": "/static/js/2.cb44da03.chunk.js"
+  },
+  {
+    "revision": "6911db604fedf1b3b856",
+    "url": "/static/js/runtime~main.6911db60.js"
+  },
+  {
+    "revision": "1d382f51096c5da4b237c3694765cf02",
+    "url": "/static/media/resume.1d382f51.pdf"
+  },
+  {
+    "revision": "10bc078260ca966d2b73ccb81a3513a8",
+    "url": "/static/media/snake.10bc0782.png"
+  },
+  {
+    "revision": "3ca4a9976c66c66b29e4efa7c55186f5",
+    "url": "/index.html"
+  }
+];
